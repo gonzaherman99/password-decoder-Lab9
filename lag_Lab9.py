23f8639b82d1a0a16a76f49a26f225d84acef5fa
Luis Gonzalez <investlagh@hotmail.com>
2024-07-27 23:17:06 -0400

    Added the decoder function

--- lag_Lab9.py
+++ lag_Lab9.py
@@ -1,3 +1,7 @@
 def encode(password):
     encoded = ''.join(str((int(char) + 3) % 10) for char in password)
     return encoded
+
+def decode(password):
+    decoded = ''.join(str((int(char) - 3) % 10) for char in password)
+    return decoded
